303c96c6fa1ee4c1c5c9066695cdd6eab92d316d
Bob Rots <bobrots@hotmail.com>
2022-01-28 21:18:42 +0100

    Updated to VS 2022, Update Exercise 1 & Presentation.

--- Beginner/C++ Beginner/Exercise 1/Exercise 1.cpp
+++ Beginner/C++ Beginner/Exercise 1/Exercise 1.cpp
@@ -4,12 +4,12 @@
     
     Create a header file and declare a function in the 'TMC' namespace which converts the 
     temperature in °F to °C. Then in the main function, which is 
-    already defined, use this function to convert from 110 °F to °C. 
+    already defined, use this function to convert from 110.2 °F to °C. 
 
     Celcius = (Fahrenheit - 32) * 5 / 9
 
     Print the result to the console by using the standard character out, std::cout. 
-    When converting from 110 °F the result should be 43.333 °C.
+    When converting from 110.2 °F the result should be 43.444 °C.
 */
 
 #include <iostream>
@@ -17,7 +17,7 @@
 
 int main()
 {
-    float result{ TMC::ConvertFahrenheitToCelcius(110.f) };
+    float value{ 110.2f };
 
-    std::cout << "110 °F is equal to " << result << " °C\n";
+    std::cout << value << " °F is equal to " << TMC::ConvertFahrenheitToCelcius(value) << " °C\n";
 }
