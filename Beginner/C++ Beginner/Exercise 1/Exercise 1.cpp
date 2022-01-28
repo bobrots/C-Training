a318045823695d8dcf67bc3c2a522a56deabfe35
Bob Rots <bobrots@hotmail.com>
2022-01-28 21:20:42 +0100

    Update Exercise 1.

--- Beginner/C++ Beginner/Exercise 1/Exercise 1.cpp
+++ Beginner/C++ Beginner/Exercise 1/Exercise 1.cpp
@@ -2,12 +2,14 @@
 /*
     Exercise 1:
     
-    Create a function to converts the temperature from degrees Fahrenheit to degrees Celcius. 
-    Call this function from main and supply it the value 110.2.
+    Create a header file and declare a function in the 'TMC' namespace which converts the 
+    temperature in °F to °C. Then in the main function, which is 
+    already defined, use this function to convert from 110.2 °F to °C. 
 
     Celcius = (Fahrenheit - 32) * 5 / 9
 
-    Print the result to the console. 
+    Print the result to the console by using the standard character out, std::cout. 
+    When converting from 110.2 °F the result should be 43.444 °C.
 */
 
 #include <iostream>
